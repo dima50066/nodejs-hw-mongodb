a01ea58c8727246bdf781525c70cbf8143224279
Dumitru <dima50066@gmail.com>
2024-09-16 21:34:29 +0200

    step 6

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import { env } from './utils/env.js';
 import { getContacts } from './services/contacts.js';
+import { getContactsById } from './services/contacts.js';
+
+dotenv.config();
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
+
   app.use(express.json());
+
   app.use(cors());
+
   app.use(
     pino({
       transport: {
@@ -18,17 +25,32 @@ export const setupServer = () => {
     }),
   );
 
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getContacts();
+    console.log(contacts);
+
+    res.status(200).json({ data: contacts });
+  });
+
+  app.get('/contacts/:id', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactsById(contactId);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Not found',
+      });
+    } else {
+      res.status(200).json({ data: contact });
+    }
+  });
+
   app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getContacts();
-    res.status(200).json({ data: contacts });
   });
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -8,3 +8,12 @@ export const getContacts = async () => {
     console.log('Error: fetching contacts', error);
   }
 };
+
+export const getContactsById = async (id) => {
+  try {
+    const contact = await StudentsCollection.findOne({ _id: id });
+    return contact;
+  } catch (error) {
+    console.log('Error: fetching contacts', error);
+  }
+};
