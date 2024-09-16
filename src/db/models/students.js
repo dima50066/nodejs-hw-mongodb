e6aecf3efd2fbedbd28d27ebd09b655e24d8565d
Dumitru <dima50066@gmail.com>
2024-09-16 19:09:18 +0200

    step 4

---
+++ src/db/models/students.js
@@ -0,0 +1,34 @@
+// src/db/models/student.js
+
+import { Schema, model } from 'mongoose';
+
+const studentsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const StudentsCollection = model('Students', studentsSchema);
