3f82da8b6acda18cc89b7940d8684707b7887aba
Dumitru <dima50066@gmail.com>
2024-09-19 15:50:25 +0200

    step 2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,21 +7,45 @@ import {
   upsertContactController,
   patchContactController,
 } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+
+import { isValidId } from '../middlewares/isValidId.js';
+
 const router = new Router();
+
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:id', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:id', isValidId, ctrlWrapper(getContactByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
 
-router.delete('/contacts/:id', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:id', isValidId, ctrlWrapper(deleteContactController));
 
-router.put('/contacts/:id', ctrlWrapper(upsertContactController));
+router.put(
+  '/contacts/:id',
+  isValidId,
+  validateBody(createContactSchema),
+  ctrlWrapper(upsertContactController),
+);
 
-router.patch('/contacts/:id', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:id',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,40 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().required().messages({
+    'string.base': 'Name must be a string',
+    'string.min': 'Name must be at least 3 characters long',
+    'string.max': 'Name must be at most 20 characters long',
+    'string.empty': 'Name cannot be an empty field',
+    'any.required': 'Name is a required field',
+  }),
+  phoneNumber: Joi.string().required().messages({
+    'string.base': 'Phone number must be a number',
+    'string.empty': 'Phone number cannot be an empty field',
+    'string.min': 'Phone number must be at least 3 characters long',
+    'string.max': 'Phone number must be at most 20 characters long',
+  }),
+  email: Joi.string().messages({
+    'string.base': 'Email must be a string',
+    'string.empty': 'Email cannot be an empty field',
+    'string.email': 'Email must be a valid email address',
+    'string.min': 'Phone number must be at least 3 characters long',
+    'string.max': 'Phone number must be at most 20 characters long',
+  }),
+  contactType: Joi.string().valid('work', 'home', 'personal').messages({
+    'string.base': 'Contact type must be a string',
+    'any.only':
+      'Contact type must be one of the following: work, home, personal',
+    'string.min': 'Phone number must be at least 3 characters long',
+    'string.max': 'Phone number must be at most 20 characters long',
+  }),
+  isFavourite: Joi.boolean().default(false),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  phoneNumber: Joi.string(),
+  email: Joi.string(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+  isFavourite: Joi.boolean(),
+});
