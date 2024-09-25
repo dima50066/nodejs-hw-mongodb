35ab5bd6fc1c2847df15479bf46bdd232954a64f
Dumitru <dima50066@gmail.com>
2024-09-25 20:36:42 +0200

    add app.get "/"

--- src/server.js
+++ src/server.js
@@ -6,20 +6,26 @@ import {
   getContactByIdController,
 } from './controllers/contacts.js';
 
+const PORT = process.env.PORT || 3000;
+
 export const setupServer = () => {
   const app = express();
 
   app.use(cors());
   app.use(pino());
 
+  app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Contacts API' });
+  });
+
   app.get('/contacts', getContactsController);
+
   app.get('/contacts/:contactId', getContactByIdController);
 
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
   });
 
-  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
