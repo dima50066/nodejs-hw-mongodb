2cd939189ad70f2f37771349e8f5e8836b206460
Dumitru <dima50066@gmail.com>
2024-09-25 20:57:30 +0200

    work with project, add app.get"/"

--- src/server.js
+++ src/server.js
@@ -24,6 +24,10 @@ export const setupServer = () => {
     }),
   );
 
+  app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Contacts API' });
+  });
+
   app.use(contactsRouter);
 
   app.use(notFoundHandler); // Обробка 404
