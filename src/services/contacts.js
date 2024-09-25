fe1ddb872acedd8e6bd8fcad4c806fe6af4b74d5
Dumitru <dima50066@gmail.com>
2024-09-25 17:45:02 +0200

    add fixes from mentor

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,20 +6,51 @@ export const getContacts = async () => {
     console.log('Fetching contacts', contacts);
 
     if (!contacts || contacts.length === 0) {
-      console.log('No contacts found');
+      return {
+        status: 404,
+        message: 'No contacts found',
+        data: [],
+      };
     }
-    return contacts;
+
+    return {
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    };
   } catch (error) {
     console.log('Error: fetching contacts', error);
-    throw error;
+    throw {
+      status: 500,
+      message: 'Error fetching contacts',
+      error,
+    };
   }
 };
 
 export const getContactsById = async (id) => {
   try {
     const contact = await ContactsCollection.findById(id);
-    return contact;
+
+    if (!contact) {
+      return {
+        status: 404,
+        message: `Contact with id ${id} not found`,
+        data: null,
+      };
+    }
+
+    return {
+      status: 200,
+      message: `Successfully found contact with id ${id}!`,
+      data: contact,
+    };
   } catch (error) {
-    console.log('Error: fetching contacts with ID ${id}', error);
+    console.log(`Error: fetching contact with ID ${id}`, error);
+    throw {
+      status: 500,
+      message: `Error fetching contact with id ${id}`,
+      error,
+    };
   }
 };
